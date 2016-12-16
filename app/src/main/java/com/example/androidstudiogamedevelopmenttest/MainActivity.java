226ef0a91d5bd6992a0ca3b344ffa46460c1d269
Sten Kamerling <stjaka@hotmail.com>
2016-12-16 16:42:09 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/androidstudiogamedevelopmenttest/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.androidstudiogamedevelopmenttest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+    private String myProperty;
+
+    public String getMyProperty() {
+        return myProperty;
+    }
+
+    public void setMyProperty(String myProperty) {
+        this.myProperty = myProperty;
+    }
+
+    private void DoSomething(String someValue){
+        setMyProperty(someValue);
+    }
